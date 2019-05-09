db63860b43afe423987586af4f3af21c78ed3bae
yang <yang.li@rethinkwith.me>
2019-05-08 20:58:16 -0400

    Project configuration * Enabled mongodb http session store * Added web security config * Added mongodb connection properties in application.yaml

---
+++ src/main/java/com/example/demo/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.session.data.mongo.JacksonMongoSessionConverter;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+
+@EnableMongoHttpSession
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory, MongoConverter mongoConverter) {
+        return new MongoTemplate(mongoDbFactory, mongoConverter);
+    }
+
+    @Bean
+    public JacksonMongoSessionConverter mongoSessionConverter() {
+        return new JacksonMongoSessionConverter();
+    }
+}
